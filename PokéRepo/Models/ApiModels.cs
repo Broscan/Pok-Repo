c587528e1d55d86348efa4e5bb9c54a7c5e9363f
Broscan <Calle992015@gmail.com>
2024-02-07 11:57:34 +0100

    hej

--- PokéRepo/Models/ApiModels.cs
+++ PokéRepo/Models/ApiModels.cs
@@ -1,21 +1,80 @@
-﻿namespace PokéRepo.Models
+﻿using Newtonsoft.Json;
+
+namespace PokéRepo.Models
 {
     public class ApiModels
     {
         public class Root
         {
-            public int Count { get; set; }
-            public string? Next { get; set; }
-            public int Previous { get; set; }
-            public List<Result> Results { get; set; }
+            [JsonProperty("abilities")]
+            public List<Ability> Abilities { get; set; }
+
+            [JsonProperty("base_experience")]
+            public int? BaseExperience { get; set; }
+
+            [JsonProperty("forms")]
+            public List<Form> Forms { get; set; }
+
+
+            [JsonProperty("height")]
+            public int? Height { get; set; }
+
+
+            [JsonProperty("id")]
+            public int? Id { get; set; }
+
+
+            [JsonProperty("location_area_encounters")]
+            public string LocationAreaEncounters { get; set; }
+
+
+            [JsonProperty("name")]
+            public string Name { get; set; }
+
+
+
+
+
+
+
+
+            [JsonProperty("sprites")]
+            public Sprites Sprites { get; set; }
+
+            [JsonProperty("stats")]
+            public List<Stat> Stats { get; set; }
+
+            [JsonProperty("weight")]
+            public int? Weight { get; set; }
         }
 
-        public class Result
+        public class Sprites
         {
-            public string? Name { get; set; }
-            public string? Url { get; set; }
-        }
+            [JsonProperty("back_default")]
+            public string BackDefault { get; set; }
 
+            [JsonProperty("back_female")]
+            public string BackFemale { get; set; }
+
+            [JsonProperty("back_shiny")]
+            public string BackShiny { get; set; }
+
+            [JsonProperty("back_shiny_female")]
+            public string BackShinyFemale { get; set; }
+
+            [JsonProperty("front_default")]
+            public string FrontDefault { get; set; }
+
+            [JsonProperty("front_female")]
+            public string FrontFemale { get; set; }
+
+            [JsonProperty("front_shiny")]
+            public string FrontShiny { get; set; }
+
+            [JsonProperty("front_shiny_female")]
+            public string FrontShinyFemale { get; set; }
+
+        }
 
 
 
